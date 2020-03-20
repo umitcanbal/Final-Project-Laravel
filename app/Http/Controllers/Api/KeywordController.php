70089e2ea3bcbaa7f83b3ca01f767721b10ccf81
umitcanbal <umitcanbal@gmail.com>
2020-03-20 20:34:57 +0100

    trying

--- app/Http/Controllers/Api/KeywordController.php
+++ app/Http/Controllers/Api/KeywordController.php
@@ -11,24 +11,28 @@ use App\Video;
 class KeywordController extends Controller
 {
     public function getTheVideos() {
-        // $keyword_id = 1;
-        // $keyword = Keyword::findOrFail($keyword_id);
-        // $videos = $keyword->videos()->get();
-        // return [
-        //     $videos,
-        //     $keyword
-        // ];
-
-        $keywords = Keyword::all();
-        $videos = [];
-        foreach($keywords as $keyword) {
-            array_push($videos, $keyword->videos()->get());
-        }
-
+        $keyword_id = 1;
+        $keyword = Keyword::findOrFail($keyword_id);
+        $videos = $keyword->videos()->get();
         return [
             $videos,
-            $keywords,
+            $keyword
         ];
+
+        // $keywords = Keyword::all();
+        // $videos = [];
+        // foreach($keywords as $keyword) {
+        //     array_push($videos, $keyword->videos()->get());
+        // }
+
+        // return [
+        //     $videos,
+        //     $keywords,
+        // ];
+
+        ANDREJ
+
+        //iki controller, keyword_id =1 için mesela, videoları da keyword_id ye göre çekicez. Keyword_id KULLANICININ TIKLADIĞI KELİME BELİRLEYECEK
     }
 
     
