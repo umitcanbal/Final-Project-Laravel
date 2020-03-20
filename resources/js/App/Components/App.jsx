dafb9bf4643e8983170ced9a38fb42b7037be6c0
umitcanbal <umitcanbal@gmail.com>
2020-03-20 19:32:57 +0100

    react-laravel connected, components are created but they dont really work yet

---
+++ resources/js/App/Components/App.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import Dropdown from "./Dropdown.jsx";
+import Video from "./Video.jsx";
+
+
+export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            keywords: null,
+            videos: null,
+        }
+    }
+
+    componentDidMount() {
+        fetch("/api/something")
+            .then(response => (response.json()))
+            .then(data => this.setState({
+                keywords: data[1], 
+                videos: data[0]
+            }))
+    }
+
+    render() {
+        return (
+            <>
+                <h1>Application component</h1>
+
+                
+            </>
+        )
+
+        // return (
+        //     <>
+        //         <Dropdown keywords={this.state.keywords}/>
+        //         <Video videos={this.state.videos}/>
+        //     </>
+        // )
+    }
+}
