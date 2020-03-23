1c629d15f34f6a14fcb5dc79d22353423a028cbe
umitcanbal <umitcanbal@gmail.com>
2020-03-23 12:51:34 +0100

    keyword list is fetched in KeywordList and rendered in Keyword

--- resources/js/App/Components/LayoutLeft/KeywordList/Keyword/Keyword.jsx
+++ resources/js/App/Components/LayoutLeft/KeywordList/Keyword/Keyword.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-import "./keyword.css"
+import "./keyword.css";
 
 export default class Keyword extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return(
       <div className="keyword">
         <p>Writing from Keyword</p>
+        <div>
+          {this.props.keywordList.map( keyword => {
+            return <div key={keyword.id}>{keyword.name}</div>
+          } )}
+        </div>
       </div>
     )
   }
--- resources/js/App/Components/LayoutLeft/KeywordList/KeywordList.jsx
+++ resources/js/App/Components/LayoutLeft/KeywordList/KeywordList.jsx
@@ -4,11 +4,30 @@ import "./keywordlist.css";
 import Keyword from "./Keyword/Keyword.jsx";
 
 export default class KeywordList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      keywordList: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch("api/keywords")
+      .then( (response) => { return (response.json()) } )
+      .then( (data) => { this.setState( { keywordList: data } ) } )
+  }
+
   render() {
+    const { keywordList } = this.state;
     return(
       <div className="keywordlist">
         <p>Writing from KeywordList</p>
-        <Keyword />
+        {
+          keywordList.length > 0 
+          &&
+          <Keyword keywordList={keywordList}/>
+        }
       </div>
     )
   }
