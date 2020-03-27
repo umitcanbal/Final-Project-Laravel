211a2ecdbb2149afe37d6b4db765914b94ac9212
umitcanbal <umitcanbal@gmail.com>
2020-03-27 16:02:23 +0100

    after Tim restructured"

--- resources/js/App/Components/LayoutRight/VideoPage/Videopage.jsx
+++ resources/js/App/Components/LayoutRight/VideoPage/Videopage.jsx
@@ -8,13 +8,11 @@ export default class VideoPage extends React.Component {
 
     this.state = {
       currentVideo: 0,
-      changed: false
     }
   }
 
   componentDidUpdate(prevProps) {
     if(this.props.match.params.alias !== prevProps.match.params.alias) {
-      console.log("update has started");
       { this.setState( { currentVideo: 0, changed: true } ) }
     }
   }
@@ -24,25 +22,49 @@ export default class VideoPage extends React.Component {
   }
 
   render() {
-    console.log("render has started")
-    console.log("this.state.changed", this.state.changed);
-    
     const { keywordList } = this.props;
     const  { videos }  = keywordList.find((word) => {
       return (word.alias === this.props.match.params.alias)
     });
-    console.log("state.currentvideo", this.state.currentVideo)
 
     return (
       <div>
-              {this.state.changed && this.state.currentVideo != 0 ? <div>loading</div> :  <div>
-            <div dangerouslySetInnerHTML={{ __html: `<iframe width="560" height="315" src="https://www.youtube.com/embed/${videos[this.state.currentVideo] ? videos[this.state.currentVideo].URL : ""}?controls=1&cc_load_policy=1&cc_lang_pref=en&start=${(videos[this.state.currentVideo] ? videos[this.state.currentVideo].pivot.offset_start : "")-5}&end=${(videos[this.state.currentVideo] ? videos[this.state.currentVideo].pivot.offset_start : '')+5}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>` }} />
-
+        {this.state.changed && this.state.currentVideo != 0 ? <div>loading</div> : 
+          <div> 
+            <div dangerouslySetInnerHTML={{ __html: getIframeCode(videos[this.state.currentVideo]) }} />
             <button onClick={this.nextVideo}>Next</button>
           </div>}
-
-         
       </div>
     )
   }
+}
+
+
+function getIframeCode(currentVideo) {
+  const url = currentVideo ? currentVideo.URL : ""
+  const offsetStart = getOffsetStart(currentVideo)
+  const offsetFinish = offsetStart + 5
+
+  const parameters = {
+    cc_load_policy: 1,
+    cc_lang_pref: 'en',
+    start: offsetStart,
+    end: offsetFinish
+  }
+
+  return  `<iframe width="560" height="315" src="https://www.youtube.com/embed/${url}${getEmbedParametersString(parameters)}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+
+function getOffsetStart(currentVideo) {
+  return Math.max(((currentVideo ? currentVideo.pivot.offset_start : 0) - 5), 0)
+}
+
+function getEmbedParametersString(parameters) {
+  const parametersMap = Object.entries(parameters)
+  if (!parametersMap.length) return ''
+
+  return `?${parametersMap.map(([key, value], index) => {
+    const keyValueString = `${key}=${value}`
+    return index === parametersMap.length - 1 ? keyValueString : keyValueString + '&'
+  })}`
 }
