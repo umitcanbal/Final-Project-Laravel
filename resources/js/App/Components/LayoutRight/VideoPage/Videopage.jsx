4b5a7ce9b9d4e1e56bba30ab71b48c914a7d96f3
umitcanbal <umitcanbal@gmail.com>
2020-03-31 00:55:46 +0200

    changing keyword is fixed. Manually subtitles are added and exact keyword(just for one example) is detected by regular expressions and styled(big font size - red color)

--- resources/js/App/Components/LayoutRight/VideoPage/Videopage.jsx
+++ resources/js/App/Components/LayoutRight/VideoPage/Videopage.jsx
@@ -9,10 +9,17 @@ export default class VideoPage extends React.Component {
     this.state = {
       currentVideo: 0,
       nextButton: false,
+      changed: false,
     }
   }
 
   async componentDidMount() {
+//     let exampleJSON = '{"start_time":"00:09:31,583","end_time":"00:09:35,747","text":"There's a nuclear holocaust.\nl'm the last man on earth."}';
+
+// let exampleDecoded = JSON.parse(exampleJSON);
+// console.log(JSON.parse('{ "start_time":"00:09:31,583", "end_time":"00:09:35,747", "text":"There\'s a nuclear holocaust"}'));,
+var obj = JSON.parse('{ "start_time":"00:09:31,583", "end_time":"00:09:35,747", "text":"There\'s a nuclear holocaust"}');
+console.log(obj);
     const { keywordList } = this.props;
     const { videos } = await keywordList.find((word) => {
       return (word.alias === this.props.match.params.alias)
@@ -20,10 +27,20 @@ export default class VideoPage extends React.Component {
     this.setVideoPlayer(videos[this.state.currentVideo])
   }
 
-  componentDidUpdate(prevProps) {
+  async componentDidUpdate(prevProps) {
     if (this.props.match.params.alias !== prevProps.match.params.alias) {
-      { this.setState({ currentVideo: 0, changed: true }) }
+    await this.setState({ currentVideo: 0, changed: true });
+
+      const { keywordList } = this.props;
+      const { videos } = await keywordList.find((word) => {
+        return (word.alias === this.props.match.params.alias)
+      });
+
+      setTimeout(() => {
+        this.setVideoPlayer(videos[this.state.currentVideo]);
+      }, 700);
     }
+
   }
 
   async nextVideo() {
@@ -32,76 +49,65 @@ export default class VideoPage extends React.Component {
     const { videos } = await keywordList.find((word) => {
       return (word.alias === this.props.match.params.alias)
     });
-    
-    // player.destroy();
-    
+
     setTimeout(() => {
       this.setVideoPlayer(videos[this.state.currentVideo]);
     }, 500);
-    
   }
 
   setVideoPlayer = async (currentVideo) => {
-    console.log("tekrardan video player")
     const url = currentVideo ? currentVideo.URL : ""
   
     const offsetStart = getOffsetStart(currentVideo)
     const offsetFinish = offsetStart + 10
 
     const parameters = {
-      
       cc_load_policy: 1,
       cc_lang_pref: 'en',
       start: offsetStart,
       end: offsetFinish,
       rel: 0,
     }
-    
-    await this.setState({nextButton: false});
-
 
     player = await window.getYoutubePlayer(url, parameters);
-    
-    
-    // console.log(player);
     player.playVideo();
-    
-    
 
-    const timeDetector = setInterval(() => { // TODO Clear all intervals when necessary! (component unmounting, switching to another video)
-      this.setState({
-        currentVideoTime: player.getCurrentTime()
-      })
+    await this.setState({nextButton: false});
+    
+    const timeDetector = setInterval(() => {
 
-      if(this.state.nextButton) {
-        console.log("next butonunun içindeyim")
-        // player.parentNode.removeChild(player)
-        // window.removeChild(player);
-        player.destroy(player);
-        // console.log(player);
-        clearInterval(timeDetector);
-        console.log("next butonuna bastığın için videoyu durdurdum");
+      if(player.getPlayerState()) {
+        this.setState({
+          currentVideoTime: player.getCurrentTime(),
+        })
       }
+
+      if(this.state.nextButton || this.state.changed) {
+        clearInterval(timeDetector);
+        player.destroy();
+        this.setState({ changed: false })
+      }
+
     }, 500)
 
-    // function onPlayerStateChange() {
-    //   if('state is stopped') clearIn
-    // }
   }
 
   render() {
-    // const { keywordList } = this.props;
-    // const { videos } = keywordList.find((word) => {
-    //   return (word.alias === this.props.match.params.alias)
-    // });
-    console.log(this.state);
-    // const { currentVideoTime } = this.state
-    // console.log('current', currentVideoTime)
+    let text = "There's a nuclear holocaust.\nl'm the last man on earth. Would you go out with me?";
+    console.log(this.props.keywordList[0].name);
+    let highlight = this.props.keywordList[0].id;
+
+    function getHighlightedText(text, highlight) {
+      // Split text on highlight term, include term itself into parts, ignore case
+      const parts = text.split(new RegExp(`(${highlight})`, 'gi'));
+      return <p>{parts.map(part => part.toLowerCase() === highlight.toLowerCase() ? <span style={{ color: "red", fontSize: "3rem"}}>{part.toUpperCase()}</span> : part)}</p>;
+  }
     return (
       <div>
         {this.state.changed && this.state.currentVideo != 0 ? <div>loading</div> :
           <div>
             <div id='player'></div>
+            <>{getHighlightedText(text, highlight)}</>
             <button onClick={this.nextVideo.bind(this)}>Next</button>
             <p style={{ fontWeight: 'bold' }}>My Text</p>
           </div>}
@@ -119,3 +125,5 @@ export default class VideoPage extends React.Component {
 function getOffsetStart(currentVideo) {
   return Math.max(((currentVideo ? currentVideo.pivot.offset_start : 0) - 5), 0)
 }
+
+
